590df12b75825c4936a8f5f6ee8a81dc9aa562a6
Rishabh Raj <rishabh.raaj17@gmail.com>
2020-11-27 00:41:44 +0100

    optimization_phase1

--- unsupervised_tp_0/extracted_of_optimization.py
+++ unsupervised_tp_0/extracted_of_optimization.py
@@ -1,8 +1,11 @@
 import torch
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 from average_image.constants import SDDVideoClasses
+from average_image.utils import is_inside_bbox
 from log import get_logger, initialize_logging
 
 initialize_logging()
@@ -39,19 +42,62 @@ def least_indices(array: np.ndarray, n: int) -> tuple:
     return np.unravel_index(indices, array.shape)
 
 
-def cost_function(of_points, center, top_k):
+def cost_function(of_points, center, top_k, alpha, bbox, weight_points_inside_bbox_more=True):
+    points_inside_bbox_idx = []
+    if weight_points_inside_bbox_more:
+        points_inside_bbox = [is_inside_bbox(point, bbox) for point in of_points]
+        points_inside_bbox_idx = (np.array(points_inside_bbox) > 0).nonzero()[0]
+    center = np.expand_dims(center, axis=0)
     num_points, dim = of_points.shape
     center_stacked = np.repeat(center, repeats=num_points, axis=0)
-    dist = np.linalg.norm((of_points - center_stacked), ord=2, axis=1)
+    dist = np.linalg.norm((of_points - center_stacked), ord=2, axis=1, keepdims=True)
     z = (1 / dist).sum()
-    weighted_of_points = (1 / dist) * of_points
-    weighted_of_points = (1 / z) * weighted_of_points
+    weighted_of_points = np.power((1 / dist) * of_points, alpha)
+    weighted_of_points = (1 / z) * weighted_of_points  # normalize 0-1?
+    if weight_points_inside_bbox_more and len(points_inside_bbox_idx) > 0:
+        weighted_of_points[points_inside_bbox_idx] = weighted_of_points[points_inside_bbox_idx] * 100
     top_k_indices = largest_indices(weighted_of_points, top_k)[0]
-    return weighted_of_points, weighted_of_points[top_k_indices]
+    return weighted_of_points, of_points[top_k_indices]
 
 
-def of_optimization(data_features, bounding_box, bounding_box_centers):
-    print()
+def of_optimization(data_features_x, data_features_y, bounding_box_x, bounding_box_y, bounding_box_centers_x,
+                    bounding_box_centers_y, alpha):
+    for features_x, bboxs_x, bbox_centers_x, features_y, bboxs_y, bbox_centers_y in \
+            zip(data_features_x, bounding_box_x, bounding_box_centers_x, data_features_y, bounding_box_y,
+                bounding_box_centers_y):
+        for feature_x, bbox_x, bbox_center_x, feature_y, bbox_y, bbox_center_y in \
+                zip(features_x, bboxs_x, bbox_centers_x, features_y, bboxs_y, bbox_centers_y):
+            t0_points_xy, t0_points_uv, t0_points_past_uv = feature_x[:, :2], feature_x[:, 2:4], feature_x[:, 4:]
+            t1_points_xy, t1_points_uv, t1_points_past_uv = feature_y[:, :2], feature_y[:, 2:4], feature_y[:, 4:]
+            t0_x_min, t0_y_min, t0_x_max, t0_y_max = bbox_x
+            t0_x, t0_y, t0_w, t0_h = t0_x_min, t0_y_min, (t0_x_max - t0_x_min), (t0_y_max - t0_y_min)
+            t1_x_min, t1_y_min, t1_x_max, t1_y_max = bbox_y
+            t1_x, t1_y, t1_w, t1_h = t1_x_min, t1_y_min, (t1_x_max - t1_x_min), (t1_y_max - t1_y_min)
+
+            shifted_points = t0_points_xy + t0_points_uv
+
+            weighted_shifted_points, weighted_shifted_points_top_k = cost_function(shifted_points, bbox_center_y,
+                                                                                   top_k=10, alpha=alpha, bbox=bbox_y,
+                                                                                   weight_points_inside_bbox_more=True)
+
+            plot_basic_analysis(bbox_center_y, shifted_points, t1_h, t1_points_xy, t1_w, t1_x, t1_y,
+                                weighted_shifted_points_top_k)
+
+
+def plot_basic_analysis(bbox_center_y, shifted_points, t1_h, t1_points_xy, t1_w, t1_x, t1_y,
+                        weighted_shifted_points_top_k=None):
+    plt.plot(shifted_points[:, 0], shifted_points[:, 1], 'o', markerfacecolor='blue', markeredgecolor='k',
+             markersize=8, label='OF Shifted')
+    plt.plot(t1_points_xy[:, 0], t1_points_xy[:, 1], '*', markerfacecolor='green', markeredgecolor='k',
+             markersize=8, label='GT Points')
+    plt.plot(bbox_center_y[0], bbox_center_y[1], '+', markerfacecolor='black', markeredgecolor='k',
+             markersize=8, label='Bbox Center')
+    if weighted_shifted_points_top_k is not None:
+        plt.plot(weighted_shifted_points_top_k[:, 0], weighted_shifted_points_top_k[:, 1], '*', markerfacecolor='pink',
+                 markeredgecolor='k', markersize=8, label='Weighted TopK')
+    plt.gca().add_patch(Rectangle((t1_x, t1_y), t1_w, t1_h, linewidth=1, edgecolor='r', facecolor='none'))
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -69,12 +115,12 @@ if __name__ == '__main__':
     dataset_feats = torch.load(save_path + file_name)
     logger.info('Dataset Loaded')
 
-    dataset_x, dataset_y, dataset_frames, dataset_track_ids, dataset_center_x, dataset_center_y, \
-    dataset_bbox_x, dataset_bbox_y, dataset_center_data = dataset_feats['x'], dataset_feats['y'], \
-                                                          dataset_feats['frames'], \
-                                                          dataset_feats['track_ids'], dataset_feats[
-                                                              'bbox_center_x'], \
-                                                          dataset_feats['bbox_center_y'], dataset_feats[
-                                                              'bbox_x'], \
-                                                          dataset_feats['bbox_y'], dataset_feats['center_based']
-    of_optimization(data_features=dataset_x, bounding_box=dataset_bbox_x, bounding_box_centers=dataset_center_x)
+    dataset_x, dataset_y, dataset_frames, dataset_track_ids, dataset_center_x, dataset_center_y, dataset_bbox_x, \
+    dataset_bbox_y, dataset_center_data = dataset_feats['x'], dataset_feats['y'], dataset_feats['frames'], \
+                                          dataset_feats['track_ids'], dataset_feats['bbox_center_x'], \
+                                          dataset_feats['bbox_center_y'], dataset_feats['bbox_x'], \
+                                          dataset_feats['bbox_y'], dataset_feats['center_based']
+
+    of_optimization(data_features_x=dataset_x, data_features_y=dataset_y, bounding_box_x=dataset_bbox_x,
+                    bounding_box_y=dataset_bbox_y, bounding_box_centers_x=dataset_center_x,
+                    bounding_box_centers_y=dataset_center_y, alpha=1)
