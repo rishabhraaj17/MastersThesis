746b6c9c7cec8a1343f445a7079907ed2941dbb2
Rishabh Raj <rishabh.raaj17@gmail.com>
2021-06-09 12:35:10 +0200

    new method for multiple batch size - 2

--- src/position_maps/interact.py
+++ src/position_maps/interact.py
@@ -239,6 +239,22 @@ def interact_demo(cfg):
 
     torch.autograd.set_detect_anomaly(True)
 
+    if cfg.interact.batch_size <= 1:
+        one_item_per_batch(cfg, in_dxdy, in_xy, killed_tracks, last_frame_number, last_iter_blobs, loss_fn, model, opt,
+                           opt_traj, out_xy, position_map_network_type, running_tracks, step_counter, total_traj_loss,
+                           track_ids_used, train_loader)
+    else:
+        multiple_items_per_batch(cfg=cfg, in_dxdy=in_dxdy, in_xy=in_xy, killed_tracks=killed_tracks,
+                                 last_frame_number=last_frame_number, last_iter_blobs=last_iter_blobs, loss_fn=loss_fn,
+                                 model=model, opt=opt, opt_traj=opt_traj, out_xy=out_xy,
+                                 position_map_network_type=position_map_network_type, running_tracks=running_tracks,
+                                 step_counter=step_counter, total_traj_loss=total_traj_loss,
+                                 track_ids_used=track_ids_used, train_loader=train_loader)
+
+
+def one_item_per_batch(cfg, in_dxdy, in_xy, killed_tracks, last_frame_number, last_iter_blobs, loss_fn, model, opt,
+                       opt_traj, out_xy, position_map_network_type, running_tracks, step_counter, total_traj_loss,
+                       track_ids_used, train_loader):
     for epoch in range(cfg.interact.num_epochs):
         opt_traj.zero_grad()
         model.train()
@@ -283,6 +299,247 @@ def interact_demo(cfg):
 
             traj_loss = torch.tensor(0, dtype=torch.float32, device=cfg.interact.device)
             if t_idx > 0:
+                current_frame_number = meta[0]['item']
+                blob_distance_matrix = np.zeros((last_iter_blobs.shape[0], blobs.shape[0]))
+                for p_idx, prev_blob in enumerate(last_iter_blobs):
+                    for b_idx, blob in enumerate(blobs):
+                        blob_distance_matrix[p_idx, b_idx] = np.linalg.norm((prev_blob - blob), ord=2)
+
+                # Hungarian
+                match_rows, match_cols = scipy.optimize.linear_sum_assignment(blob_distance_matrix)
+
+                last_track_id_used = track_ids_used[-1] if len(track_ids_used) != 0 else 0
+                for r, c in zip(match_rows, match_cols):
+                    in_xy.append(last_iter_blobs[r])
+                    out_xy.append(blobs[c])
+                    in_dxdy.append(blobs[c] - last_iter_blobs[r])
+                    is_part_of_live_track = [(i.locations[-1] == last_iter_blobs[r]).all()
+                                             and (i.frames.shape[0] == step_counter) for i in running_tracks]
+
+                    if np.array(is_part_of_live_track).any():
+                        track_idx = np.where(is_part_of_live_track)[0].item()
+                        running_tracks[track_idx].frames = np.append(running_tracks[track_idx].frames,
+                                                                     [current_frame_number])
+                        running_tracks[track_idx].locations = np.append(running_tracks[track_idx].locations,
+                                                                        [blobs[c]], axis=0)
+                    else:
+                        running_tracks.append(PositionBasedTrack(
+                            idx=last_track_id_used,
+                            frames=np.array([last_frame_number, current_frame_number]),
+                            locations=np.array([last_iter_blobs[r], blobs[c]])))
+
+                        track_ids_used.append(last_track_id_used)
+                        last_track_id_used += 1
+
+                # plot_to_debug(last_iter_output.cpu().squeeze().sigmoid().round(), txt=f'Pred Mask @ T = {t_idx - 1}')
+
+                in_xy, in_dxdy, out_xy = np.stack(in_xy), np.stack(in_dxdy), np.stack(out_xy)
+                in_xy, in_dxdy, out_xy = torch.from_numpy(in_xy), torch.from_numpy(in_dxdy), torch.from_numpy(out_xy)
+                in_xy, in_dxdy, out_xy = \
+                    in_xy.float().unsqueeze(0).to(cfg.interact.device), \
+                    in_dxdy.float().unsqueeze(0).to(cfg.interact.device), \
+                    out_xy.float().unsqueeze(0).to(cfg.interact.device)
+                batch = {'in_xy': in_xy, 'in_dxdy': in_dxdy, 'out_xy': out_xy}
+
+                in_map = generate_position_map([heat_masks.shape[-2], heat_masks.shape[-1]],
+                                               in_xy.squeeze().detach().clone().cpu().numpy(),
+                                               sigma=1.5,
+                                               heatmap_shape=None,
+                                               return_combined=True, hw_mode=True)
+                true_out_map = generate_position_map([heat_masks.shape[-2], heat_masks.shape[-1]],
+                                                     out_xy.squeeze().detach().clone().cpu().numpy(),
+                                                     sigma=1.5,
+                                                     heatmap_shape=None,
+                                                     return_combined=True, hw_mode=True)
+
+                gmm = GaussianMixture(n_components=out_xy.shape[1],
+                                      n_features=out_xy.shape[-1],
+                                      mu_init=out_xy,
+                                      var_init=torch.ones_like(out_xy) * 1.5)
+
+                traj_out = model.trajectory_model(batch)
+                traj_out_xy = traj_out['out_xy']
+
+                traj_loss = - gmm.score_samples(traj_out_xy.squeeze(0)).sum()
+
+                trajectory_map = generate_position_map([heat_masks.shape[-2], heat_masks.shape[-1]],
+                                                       traj_out_xy.squeeze().detach().clone().cpu().numpy(),
+                                                       sigma=1.5,
+                                                       heatmap_shape=None,
+                                                       return_combined=True, hw_mode=True)
+
+                if epoch % cfg.interact.plot_checkpoint == 0 and epoch != 0:
+                    plot_to_debug(in_map, txt=f'Input locations @ T = {t_idx}')
+                    plot_to_debug(true_out_map, txt=f'True locations @ T = {t_idx}')
+                    plot_to_debug(trajectory_map, txt=f'Predicted locations @ T = {t_idx}')
+
+            last_iter_output = out.clone().detach()
+            last_iter_blobs = np.copy(blobs)
+            last_frame_number = meta[0]['item']  # for batch_size=1
+            # last_frame_number = [m['item'] for m in meta]
+            in_xy, in_dxdy, out_xy = [], [], []
+
+            if position_map_network_type.__name__ == 'PositionMapUNetClassMapSegmentation':
+                loss = loss_fn(out, class_maps.long().squeeze(dim=1))
+            elif position_map_network_type.__name__ == 'PositionMapUNetPositionMapSegmentation':
+                loss = loss_fn(out, position_map.long().squeeze(dim=1))
+            elif position_map_network_type.__name__ == 'PositionMapUNetHeatmapSegmentation':
+                loss = loss_fn(out, heat_masks)
+            elif position_map_network_type.__name__ == 'PositionMapStackedHourGlass':
+                loss = model.network.calc_loss(combined_hm_preds=out, heatmaps=heat_masks)
+                loss = loss.mean()
+            else:
+                loss = loss_fn(out, heat_masks)
+
+            # train_loss.append(loss.item())
+            if (t_idx != 0) and (t_idx < cfg.interact.max_trajectory_length - 1):
+                total_traj_loss += torch.log(traj_loss)  # product loes to infinity -> trying sum log
+            elif t_idx != 0:
+                total_traj_loss.backward()
+                opt_traj.step()
+                opt_traj.zero_grad()
+                # loss += total_traj_loss
+                total_traj_loss = 1
+
+            # loss += traj_loss
+            train_loss.append(loss.item())
+
+            loss.backward()
+            opt.step()
+            step_counter += 1
+
+            still_running_tracks = []
+            for r_track in running_tracks:
+                if r_track.frames.shape[0] != step_counter:
+                    killed_tracks.append(r_track)
+                elif r_track.frames.shape[0] == step_counter:
+                    still_running_tracks.append(r_track)
+
+            running_tracks = copy.deepcopy(still_running_tracks)
+
+        logger.info(f"Epoch: {epoch} | Train Loss: {np.array(train_loss).mean()}")
+
+        if epoch % cfg.interact.plot_checkpoint == 0 and cfg.interact.do_validation:
+            model.eval()
+            val_loss = []
+
+            for data in train_loader:
+                frames, heat_masks, position_map, distribution_map, class_maps, meta = data
+
+                if position_map_network_type.__name__ == 'PositionMapUNetClassMapSegmentation':
+                    frames, class_maps = frames.to(cfg.device), class_maps.to(cfg.device)
+                elif position_map_network_type.__name__ == 'PositionMapUNetPositionMapSegmentation':
+                    frames, position_map = frames.to(cfg.device), position_map.to(cfg.device)
+                elif position_map_network_type.__name__ in ['PositionMapUNetHeatmapSegmentation',
+                                                            'PositionMapStackedHourGlass']:
+                    frames, heat_masks = frames.to(cfg.device), heat_masks.to(cfg.device)
+                else:
+                    frames, heat_masks = frames.to(cfg.device), heat_masks.to(cfg.device)
+
+                with torch.no_grad():
+                    out = model(frames)
+
+                if position_map_network_type.__name__ == 'PositionMapUNetClassMapSegmentation':
+                    loss = loss_fn(out, class_maps.long().squeeze(dim=1))
+                elif position_map_network_type.__name__ == 'PositionMapUNetPositionMapSegmentation':
+                    loss = loss_fn(out, position_map.long().squeeze(dim=1))
+                elif position_map_network_type.__name__ == 'PositionMapUNetHeatmapSegmentation':
+                    loss = loss_fn(out, heat_masks)
+                elif position_map_network_type.__name__ == 'PositionMapStackedHourGlass':
+                    loss = model.network.calc_loss(combined_hm_preds=out, heatmaps=heat_masks)
+                    loss = loss.mean()
+                else:
+                    loss = loss_fn(out, heat_masks)
+
+                val_loss.append(loss.item())
+
+                random_idx = np.random.choice(cfg.interact.batch_size, 1, replace=False).item()
+
+                if position_map_network_type.__name__ in ['PositionMapUNetPositionMapSegmentation',
+                                                          'PositionMapUNetClassMapSegmentation']:
+                    pred_mask = torch.round(torch.sigmoid(out)).squeeze(dim=1).cpu()
+                    plot_predictions(frames[random_idx].squeeze().cpu().permute(1, 2, 0),
+                                     class_maps[random_idx].squeeze().cpu()
+                                     if cfg.class_map_segmentation else position_map[random_idx].squeeze().cpu(),
+                                     pred_mask[random_idx].int() * 255,
+                                     additional_text=f"{position_map_network_type.__name__} | {loss_fn._get_name()} "
+                                                     f"| Epoch: {epoch}")
+                elif position_map_network_type.__name__ == 'PositionMapUNetHeatmapSegmentation':
+                    pred_mask = torch.round(torch.sigmoid(out)).squeeze(dim=1).cpu()
+                    plot_predictions(frames[random_idx].squeeze().cpu().permute(1, 2, 0),
+                                     heat_masks[random_idx].squeeze().cpu(),
+                                     pred_mask[random_idx].int() * 255,
+                                     additional_text=f"{position_map_network_type.__name__} | {loss_fn._get_name()} "
+                                                     f"| Epoch: {epoch}")
+                elif position_map_network_type.__name__ == 'PositionMapStackedHourGlass':
+                    pred_mask = torch.round(torch.sigmoid(out)).squeeze(dim=1).cpu()
+                    plot_predictions(frames[random_idx].squeeze().cpu().permute(1, 2, 0),
+                                     class_maps[random_idx].squeeze().cpu()
+                                     if cfg.class_map_segmentation else position_map[random_idx].squeeze().cpu(),
+                                     pred_mask[-1][random_idx].int().squeeze(dim=0) * 255,
+                                     additional_text=f"{position_map_network_type.__name__} | {loss_fn._get_name()} "
+                                                     f"| Epoch: {epoch}")
+                    plot_predictions(pred_mask[-3][random_idx].int().squeeze(dim=0) * 255,
+                                     pred_mask[-2][random_idx].int().squeeze(dim=0) * 255,
+                                     pred_mask[-1][random_idx].int().squeeze(dim=0) * 255,
+                                     additional_text=f"{position_map_network_type.__name__} | {loss_fn._get_name()} "
+                                                     f"| Epoch: {epoch}\nLast 3 HeatMaps", all_heatmaps=True)
+                else:
+                    plot_predictions(frames[random_idx].squeeze().cpu().permute(1, 2, 0),
+                                     heat_masks[random_idx].squeeze().cpu(),
+                                     out[random_idx].squeeze().cpu(),
+                                     additional_text=f"{position_map_network_type.__name__} | {loss_fn._get_name()} "
+                                                     f"| Epoch: {epoch}")
+
+            logger.info(f"Epoch: {epoch} | Validation Loss: {np.array(val_loss).mean()}")
+
+
+def multiple_items_per_batch(cfg, in_dxdy, in_xy, killed_tracks, last_frame_number, last_iter_blobs, loss_fn, model,
+                             opt, opt_traj, out_xy, position_map_network_type, running_tracks, step_counter,
+                             total_traj_loss, track_ids_used, train_loader):
+    first_iteration = True
+    for epoch in range(cfg.interact.num_epochs):
+        opt_traj.zero_grad()
+        model.train()
+
+        train_loss = []
+        for t_idx, data in enumerate(train_loader):
+            opt.zero_grad()
+
+            frames, heat_masks, position_map, distribution_map, class_maps, meta = data
+
+            if position_map_network_type.__name__ == 'PositionMapUNetClassMapSegmentation':
+                frames, class_maps = frames.to(cfg.device), class_maps.to(cfg.device)
+            elif position_map_network_type.__name__ == 'PositionMapUNetPositionMapSegmentation':
+                frames, position_map = frames.to(cfg.device), position_map.to(cfg.device)
+            elif position_map_network_type.__name__ in ['PositionMapUNetHeatmapSegmentation',
+                                                        'PositionMapStackedHourGlass']:
+                frames, heat_masks = frames.to(cfg.device), heat_masks.to(cfg.device)
+            else:
+                frames, heat_masks = frames.to(cfg.device), heat_masks.to(cfg.device)
+
+            out = model.position_map_model(frames)
+
+            # plot_to_debug(frames.cpu().squeeze().permute(1, 2, 0), txt=f'RGB @ T = {t_idx}')
+            # plot_to_debug(out.cpu().detach().squeeze().sigmoid().round(), txt=f'Pred Mask @ T = {t_idx}')
+
+            agents_count_gt = [m['bbox_centers'].shape[0] for m in meta]
+            blobs_per_image, masks = extract_agents_locations(blob_threshold=cfg.interact.blob_threshold,
+                                                              mask=out.clone().detach().cpu(),
+                                                              objectness_threshold=cfg.interact.objectness_threshold)
+            detected_maps = []
+            for blob in blobs_per_image:
+                locations = correct_locations(blob)
+                detected_maps.append(generate_position_map([masks.shape[-2], masks.shape[-1]], locations, sigma=1.5,
+                                                           heatmap_shape=None,
+                                                           return_combined=True, hw_mode=True))
+
+            # plot_to_debug(detected_maps[0], txt=f'Blobs->Mask @ T = {t_idx}')
+
+            blobs = [correct_locations(b) for b in blobs_per_image]
+
+            traj_loss = torch.tensor(0, dtype=torch.float32, device=cfg.interact.device)
+            if first_iteration:
                 current_frame_number = meta[0]['item']
                 blob_distance_matrix = np.zeros((last_iter_blobs.shape[0], blobs.shape[0]))
                 for p_idx, prev_blob in enumerate(last_iter_blobs):
