9b3bf38b7fc783629f03c15ee168402345a1461f
Rishabh Raj <rishabh.raaj17@gmail.com>
2021-06-26 18:48:58 +0200

    vis transformer

--- src/models_hub/vis_trans.py
+++ src/models_hub/vis_trans.py
@@ -1,11 +1,35 @@
 from typing import Tuple, Callable, Optional
 
+import hydra
+import torch
 from mmseg.models import VisionTransformer
 from omegaconf import DictConfig
 from torch import nn
 from torch.utils.data import Dataset
 
 from src.models_hub.base import Base
+from src.models_hub.trans_unet import Conv2dReLU
+
+
+class VITSegmentationHead(nn.Module):
+    def __init__(self, config: DictConfig):
+        super(VITSegmentationHead, self).__init__()
+        self.config = config
+        in_channels = self.config.vit.seg_head.in_channels
+        out_channels = self.config.vit.seg_head.out_channels
+        kernel_size = self.config.vit.seg_head.kernel_size
+        stride = self.config.vit.seg_head.stride
+        padding = self.config.vit.seg_head.padding
+        use_bn = self.config.vit.seg_head.use_bn
+
+        self.net = nn.Sequential(Conv2dReLU(in_channels=in_channels, out_channels=out_channels,
+                                            kernel_size=kernel_size, padding=padding, stride=stride,
+                                            use_batchnorm=use_bn),
+                                 nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
+                                           kernel_size=kernel_size, padding=padding, stride=stride))
+
+    def forward(self, x):
+        return self.net(x)
 
 
 class VisionTransformerSegmentation(Base):
@@ -17,7 +41,7 @@ class VisionTransformerSegmentation(Base):
             desired_output_shape=desired_output_shape, loss_function=loss_function, collate_fn=collate_fn
         )
         self.backbone = VisionTransformer(
-            img_size=self.config.vit.img_size, patch_size=self.config.vit.patch_size,
+            img_size=tuple(self.config.vit.img_size), patch_size=self.config.vit.patch_size,
             in_channels=self.config.vit.in_channels,
             embed_dim=self.config.vit.embed_dim, depth=self.config.vit.depth, num_heads=self.config.vit.num_heads,
             mlp_ratio=self.config.vit.mlp_ratio, out_indices=self.config.vit.out_indices,
@@ -31,9 +55,38 @@ class VisionTransformerSegmentation(Base):
             with_cls_token=self.config.vit.with_cls_token, interpolate_mode=self.config.vit.interpolate_mode,
             with_cp=self.config.vit.with_cp
         )
+        self.head = VITSegmentationHead(config=self.config)
+
+    @classmethod
+    def from_config(cls, config: DictConfig, train_dataset: Dataset = None, val_dataset: Dataset = None,
+                    desired_output_shape: Tuple[int, int] = None, loss_function: nn.Module = None,
+                    collate_fn: Optional[Callable] = None):
+        return VisionTransformerSegmentation(config=config, train_dataset=train_dataset, val_dataset=val_dataset,
+                                             desired_output_shape=desired_output_shape, loss_function=loss_function,
+                                             collate_fn=collate_fn)
 
     def forward(self, x):
-        return self.backbone(x)
+        backbone_out = self.backbone(x)
+
+        out = []
+        for o in backbone_out:
+            o = o.contiguous().view(*x.shape)
+            o = self.head(o)
+            out.append(o)
+
+        return out
 
     def calculate_loss(self, pred, target):
         return self.loss_function(pred, target)
+
+
+@hydra.main(config_path="../position_maps/config", config_name="config")
+def verify_vit(cfg):
+    inp = torch.randn((2, 3, 128, 128))
+    m = VisionTransformerSegmentation.from_config(cfg)
+    o = m(inp)
+    print()
+
+
+if __name__ == '__main__':
+    verify_vit()
