3129f33232594c9d5f5982753928925cc74f74f2
Rishabh Raj <rishabh.raaj17@gmail.com>
2021-07-08 00:15:42 +0200

    more backbones and heads

--- src_lib/models_hub/segmentors.py
+++ src_lib/models_hub/segmentors.py
@@ -1,7 +1,9 @@
 from typing import Tuple, List, Optional, Callable
 
 import torch
-from mmseg.models import MobileNetV3, LRASPPHead
+from mmpose.models import TopdownHeatmapSimpleHead
+from mmseg.models import MobileNetV3, LRASPPHead, MobileNetV2, ResNetV1c, ASPPHead, FCNHead, DepthwiseSeparableASPPHead, \
+    NLHead
 from mmseg.ops import resize
 from omegaconf import DictConfig, OmegaConf
 from torch import nn
@@ -13,6 +15,10 @@ MODELS_CONFIG = OmegaConf.load('../../src/position_maps/config/model/model.yaml'
 NORM_CONF = dict(type=MODELS_CONFIG.mm_segmentator.norm.type,
                  requires_grad=MODELS_CONFIG.mm_segmentator.norm.requires_grad)
 OUT_CLASSES = 1
+ALIGN_CORNERS = MODELS_CONFIG.mm_segmentator.align_corners
+
+# https://github.com/open-mmlab/mmsegmentation/tree/master/configs/_base_/models
+# Try transformer ones
 
 BACKBONE = {
     'mobile_net_v3_large': MobileNetV3(
@@ -21,7 +27,36 @@ BACKBONE = {
         out_indices=(1, 3, 16),
         pretrained=MODELS_CONFIG.mm_segmentator.pretrained,
         init_cfg=None
-    )
+    ),
+    'mobile_net_v3_small': MobileNetV3(
+        arch='small',
+        norm_cfg=NORM_CONF,
+        out_indices=(0, 1, 12),
+        pretrained=MODELS_CONFIG.mm_segmentator.pretrained,
+        init_cfg=None
+    ),
+    'mobile_net_v2': MobileNetV2(
+        widen_factor=1.,
+        strides=(1, 2, 2, 1, 1, 1, 1),
+        dilations=(1, 1, 1, 2, 2, 4, 4),
+        out_indices=(1, 2, 4, 6),
+        norm_cfg=NORM_CONF,
+        pretrained=MODELS_CONFIG.mm_segmentator.pretrained,
+        init_cfg=None
+    ),
+    'resnet_50': ResNetV1c(
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True,
+        norm_cfg=NORM_CONF,
+        pretrained=MODELS_CONFIG.mm_segmentator.pretrained,
+        init_cfg=None
+    ),
 }
 
 HEAD = {
@@ -34,17 +69,98 @@ HEAD = {
         num_classes=OUT_CLASSES,
         norm_cfg=NORM_CONF,
         act_cfg=dict(type='ReLU'),
-        align_corners=False,
+        align_corners=ALIGN_CORNERS,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    ),
+    'lraspp_small': LRASPPHead(
+        in_channels=(16, 16, 576),
+        in_index=(0, 1, 2),
+        channels=128,
+        input_transform='multiple_select',
+        dropout_ratio=0.1,
+        num_classes=OUT_CLASSES,
+        norm_cfg=NORM_CONF,
+        act_cfg=dict(type='ReLU'),
+        align_corners=ALIGN_CORNERS,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    ),
+    'aspp_r50': ASPPHead(
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        dilations=(1, 12, 24, 36),
+        dropout_ratio=0.1,
+        num_classes=OUT_CLASSES,
+        norm_cfg=NORM_CONF,
+        align_corners=ALIGN_CORNERS,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    ),
+    'dws_aspp_r50': DepthwiseSeparableASPPHead(
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        dilations=(1, 12, 24, 36),
+        c1_in_channels=256,
+        c1_channels=48,
+        dropout_ratio=0.1,
+        num_classes=OUT_CLASSES,
+        norm_cfg=NORM_CONF,
+        align_corners=ALIGN_CORNERS,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    ),
+    'nl_head_r50': NLHead(
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        dropout_ratio=0.1,
+        reduction=2,
+        use_scale=True,
+        mode='embedded_gaussian',
+        num_classes=OUT_CLASSES,
+        norm_cfg=NORM_CONF,
+        align_corners=ALIGN_CORNERS,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    ),
+    'uper_head_r50': NLHead(
+        in_channels=[256, 512, 1024, 2048],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=OUT_CLASSES,
+        norm_cfg=NORM_CONF,
+        align_corners=ALIGN_CORNERS,
         loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
     )
 }
 
 AUX_HEAD = {
-
+    'fcn_r50': FCNHead(
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=OUT_CLASSES,
+        norm_cfg=NORM_CONF,
+        align_corners=ALIGN_CORNERS,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    )
 }
 
 SECONDARY_AUX_HEAD = {
-
+    'topdown': TopdownHeatmapSimpleHead(
+                in_channels=1024,  # 2048,
+                out_channels=1,
+                in_index=2,  # 3,
+                num_deconv_layers=2,
+                num_deconv_filters=(256, 256),
+                num_deconv_kernels=(4, 4),
+                extra=dict(final_conv_kernel=1, ),
+                align_corners=ALIGN_CORNERS,
+                loss_keypoint=dict(type='JointsMSELoss', use_target_weight=True)
+            )
 }
 
 
