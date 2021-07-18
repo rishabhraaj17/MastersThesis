aaf6dc88db99faadafbedb6fcfc406f98853d7f6
Rishabh Raj <rishabh.raaj17@gmail.com>
2021-07-18 17:01:30 +0200

    trajectory dataset - default seq collate

--- src_lib/datasets/trajectory_stgcnn.py
+++ src_lib/datasets/trajectory_stgcnn.py
@@ -10,6 +10,33 @@ from tqdm import tqdm
 
 # https://github.com/abduallahmohamed/Social-STGCNN/blob/dbbb111a0f645e4002dd4885564da226c5e0b19f/utils.py#L86
 
+def seq_collate(data):
+    (obs_seq_list, pred_seq_list, obs_seq_rel_list, pred_seq_rel_list,
+     obs_frames_list, pred_frames_list, non_linear_ped_list, loss_mask_list) = zip(*data)
+
+    _len = [len(seq) for seq in obs_seq_list]
+    cum_start_idx = [0] + np.cumsum(_len).tolist()
+    seq_start_end = [[start, end]
+                     for start, end in zip(cum_start_idx, cum_start_idx[1:])]
+
+    # Data format: batch, input_size, seq_len
+    # Network default input format: seq_len, batch, input_size
+    obs_traj = torch.cat(obs_seq_list, dim=0).permute(2, 0, 1)
+    pred_traj = torch.cat(pred_seq_list, dim=0).permute(2, 0, 1)
+    obs_traj_rel = torch.cat(obs_seq_rel_list, dim=0).permute(2, 0, 1)
+    pred_traj_rel = torch.cat(pred_seq_rel_list, dim=0).permute(2, 0, 1)
+    obs_frames = torch.cat(obs_frames_list, dim=0).permute(2, 0, 1)
+    pred_frames = torch.cat(pred_frames_list, dim=0).permute(2, 0, 1)
+    non_linear_ped = torch.cat(non_linear_ped_list)
+    loss_mask = torch.cat(loss_mask_list, dim=0)
+    seq_start_end = torch.LongTensor(seq_start_end)
+    out = [
+        obs_traj, pred_traj, obs_traj_rel, pred_traj_rel, obs_frames, pred_frames, non_linear_ped,
+        loss_mask, seq_start_end
+    ]
+
+    return tuple(out)
+
 def anorm(p1, p2):
     norm = math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
     if norm == 0:
