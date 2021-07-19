fe15ddb0e20416ef39540f2b2574fb870f73330a
Rishabh Raj <rishabh.raaj17@gmail.com>
2021-07-20 00:22:04 +0200

    extract locations for fast experiments

--- src/position_maps/locations_collector.py
+++ src/position_maps/locations_collector.py
@@ -28,10 +28,13 @@ logger = get_logger(__name__)
 
 
 class Location(object):
-    def __init__(self, frame_number: int, locations: np.ndarray, scaled_locations: np.ndarray):
+    def __init__(self, frame_number: int, locations: np.ndarray, pruned_locations: np.ndarray,
+                 scaled_locations: np.ndarray):
+        # pruned locations are scaled
         self.frame_number = frame_number
         self.locations = locations
         self.scaled_locations = scaled_locations
+        self.pruned_locations = pruned_locations
 
     def __repr__(self):
         return f"Frame: {self.frame_number}"
@@ -41,10 +44,15 @@ class Locations(object):
     def __init__(self, locations: List[Location]):
         self.locations = locations
 
+    def __repr__(self):
+        frames_yet = [loc.frame_number for loc in self.locations]
+        return f"Covered Frames{frames_yet}"
+
 
 class ExtractedLocations(object):
     def __init__(self, video_class: SDDVideoClasses, video_numbers: int,
                  shape: Tuple[int, int], scaled_shape: Tuple[int, int],
+                 padded_shape: Tuple[int, int],
                  head0: Locations, head1: Locations, head2: Optional[Locations] = None):
         self.video_class = video_class
         self.video_numbers = video_numbers
@@ -53,6 +61,7 @@ class ExtractedLocations(object):
         self.head2 = head2
         self.shape = shape
         self.scaled_shape = scaled_shape
+        self.padded_shape = padded_shape
 
 
 def viz_pred(heat_map, frame, show=False):
@@ -62,6 +71,45 @@ def viz_pred(heat_map, frame, show=False):
 
     ax[0].set_title('Predictions')
     ax[1].set_title('RGB')
+    plt.tight_layout()
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def viz_locations_for_3_heads(head0, head1, head2, image, frame_numbers, show=False):
+    fig, ax = plt.subplots(1, 3, sharex='none', sharey='none', figsize=(16, 10))
+    h0_axis, h1_axis, h2_axis = ax
+
+    h0_axis.set_title('Head 0')
+    h1_axis.set_title('Head 1')
+    h2_axis.set_title('Head 2')
+
+    num_frames = [loc.frame_number for loc in head0.locations]
+    random_frame = np.random.choice(frame_numbers, 1, replace=False).item()
+
+    h0_axis.imshow(image[frame_numbers.index(random_frame)])
+    h1_axis.imshow(image[frame_numbers.index(random_frame)])
+    h2_axis.imshow(image[frame_numbers.index(random_frame)])
+
+    frame_num_idx = [idx for idx, loc in enumerate(head0.locations) if loc.frame_number == random_frame]
+    if len(frame_num_idx) > 1:
+        frame_num_idx = [frame_num_idx[0]]
+
+    frame_num_idx = frame_num_idx[0]
+
+    h0_axis.plot(head0.locations[frame_num_idx].scaled_locations[:, 0],
+                 head0.locations[frame_num_idx].scaled_locations[:, 1],
+                 'o', markerfacecolor='r', markeredgecolor='k', markersize=4)
+    h1_axis.plot(head1.locations[frame_num_idx].scaled_locations[:, 0],
+                 head1.locations[frame_num_idx].scaled_locations[:, 1],
+                 'o', markerfacecolor='r', markeredgecolor='k', markersize=4)
+    h2_axis.plot(head2.locations[frame_num_idx].scaled_locations[:, 0],
+                 head2.locations[frame_num_idx].scaled_locations[:, 1],
+                 'o', markerfacecolor='r', markeredgecolor='k', markersize=4)
+
+    plt.suptitle(f"Frame: {random_frame}")
     plt.tight_layout()
     if show:
         plt.show()
@@ -103,8 +151,8 @@ def extract_locations(cfg):
 
     # load model
     checkpoint_path = f'{cfg.interplay_v0.use_pretrained.checkpoint.root}' \
-                          f'{cfg.interplay_v0.use_pretrained.checkpoint.path}' \
-                          f'{cfg.interplay_v0.use_pretrained.checkpoint.version}/checkpoints/'
+                      f'{cfg.interplay_v0.use_pretrained.checkpoint.path}' \
+                      f'{cfg.interplay_v0.use_pretrained.checkpoint.version}/checkpoints/'
     checkpoint_files = os.listdir(checkpoint_path)
 
     epoch_part_list = [c.split('-')[0] for c in checkpoint_files]
@@ -137,32 +185,55 @@ def extract_locations(cfg):
         with torch.no_grad():
             pred_position_maps = position_model(frames)
 
-        frame_numbers = [m['item'] for m in meta]
-
         pred_object_locations = locations_from_heatmaps(
             frames, cfg.interplay_v0.objectness.kernel,
             cfg.interplay_v0.objectness.loc_cutoff,
             cfg.interplay_v0.objectness.marker_size, pred_position_maps,
             vis_on=False)
 
-        # filter out overlapping locations
-        selected_locations_pre_pruning = pred_object_locations[cfg.interplay_v0.objectness.index_select]
-        selected_locations = []
-        for s_loc in selected_locations_pre_pruning:
-            pruned_locations, pruned_locations_idx = prune_locations_proximity_based(
-                s_loc.numpy(), cfg.eval.objectness.prune_radius)
-            selected_locations.append(torch.from_numpy(pruned_locations))
+        for h_idx, head_loc in enumerate(pred_object_locations):
+            selected_head_predictions = pred_position_maps[h_idx]
 
-        selected_head = pred_position_maps[cfg.interplay_v0.objectness.index_select]
-        pred_object_locations_scaled, heat_maps_gt_scaled = get_adjusted_object_locations(
-            selected_locations, selected_head, meta)
+            selected_locations, loc_obj = [], []
+            for s_loc, m, head_pred in zip(head_loc, meta, selected_head_predictions):
+                frame_num = m['item']
+                pruned_locations, pruned_locations_idx = prune_locations_proximity_based(
+                    s_loc.numpy(), cfg.eval.objectness.prune_radius)
+                pruned_locations = torch.from_numpy(pruned_locations)
+                selected_locations.append(pruned_locations)
 
-        viz_pred(torch.from_numpy(heat_maps_gt_scaled[0]).sigmoid(),
-                 interpolate(frames[0, None, ...], size=meta[0]['original_shape']).squeeze(0).permute(1, 2, 0),
-                 show=True)
+                pred_object_locations_scaled, heat_maps_gt_scaled = get_adjusted_object_locations(
+                    [pruned_locations], head_pred.unsqueeze(0), [m])
+
+                loc_obj.append(Location(frame_number=frame_num, locations=s_loc.numpy(),
+                                        pruned_locations=pruned_locations.numpy(),
+                                        scaled_locations=np.stack(pred_object_locations_scaled).squeeze()))
+
+            if h_idx == 0:
+                head0_locations.locations.extend(loc_obj)
+            elif h_idx == 1:
+                head1_locations.locations.extend(loc_obj)
+            elif h_idx == 2:
+                head2_locations.locations.extend(loc_obj)
+            else:
+                logger.warning('More than expected heads')
+
+        viz_locations_for_3_heads(
+            head0_locations, head1_locations, head2_locations,
+            interpolate(frames, size=meta[0]['original_shape']).squeeze(0).permute(0, 2, 3, 1),
+            frame_numbers=[m['item'] for m in meta], show=False)
 
     save_dict = {
-
+        'locations': ExtractedLocations(
+            video_class=getattr(SDDVideoClasses, cfg.single_video_mode.video_classes_to_use[0]),
+            video_numbers=cfg.single_video_mode.video_numbers_to_use[0][0],
+            shape=meta[0]['downscale_shape'],
+            scaled_shape=meta[0]['original_shape'],
+            head0=head0_locations,
+            head1=head1_locations,
+            head2=head2_locations,
+            padded_shape=(frames.shape[-2], frames.shape[-1])
+        )
     }
     filename = 'extracted_locations.pt'
     save_path = os.path.join(os.getcwd(),
