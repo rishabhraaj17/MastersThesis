9b5418180983fb2922621edd8e9f13dbe4e0f9af
Rishabh Raj <rishabh.raaj17@gmail.com>
2021-09-23 13:58:38 +0200

    final experiments - precision analysis

--- src/position_maps/csv_utils.py
+++ src/position_maps/csv_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt, patches, lines
 
 
 def csv_merge(default_path, boosted_path, nn_path, save_root_path, dump_as_markdown=True):
@@ -8,25 +9,45 @@ def csv_merge(default_path, boosted_path, nn_path, save_root_path, dump_as_markd
     nn_df = pd.read_csv(nn_path)
 
     out_df = nn_df[['class', 'number']]
+    out_df['class'] = out_df.loc[:, 'class'].str.lower()
 
-    out_df['precision'] = default_df.loc[:, 'precision']
-    out_df['classifier_precision'] = boosted_df.loc[:, 'precision']
-    out_df['nn_precision'] = nn_df.loc[:, 'precision']
+    # out_df['precision'] = default_df.loc[:, 'precision'].round(3)
+    # out_df['classifier_precision'] = boosted_df.loc[:, 'precision'].round(3)
+    # out_df['nn_precision'] = nn_df.loc[:, 'precision'].round(3)
+    #
+    # out_df['recall'] = default_df.loc[:, 'recall'].round(3)
+    # out_df['classifier_recall'] = boosted_df.loc[:, 'recall'].round(3)
+    # out_df['nn_recall'] = nn_df.loc[:, 'recall'].round(3)
+    #
+    # out_df['ade'] = default_df.loc[:, 'ade'].round(3)
+    # out_df['classifier_ade'] = boosted_df.loc[:, 'ade'].round(3)
+    # out_df['nn_ade'] = nn_df.loc[:, 'ade'].round(3)
+    #
+    # out_df['fde'] = default_df.loc[:, 'fde'].round(3)
+    # out_df['classifier_fde'] = boosted_df.loc[:, 'fde'].round(3)
+    # out_df['nn_fde'] = nn_df.loc[:, 'fde'].round(3)
 
-    out_df['recall'] = default_df.loc[:, 'recall']
-    out_df['classifier_recall'] = boosted_df.loc[:, 'recall']
-    out_df['nn_recall'] = nn_df.loc[:, 'recall']
+    out_df['vanilla_precision'] = default_df.loc[:, 'precision'].round(3)
+    out_df['classic_precision'] = boosted_df.loc[:, 'precision'].round(3)
+    out_df['nn_precision'] = nn_df.loc[:, 'precision'].round(3)
 
-    out_df['ade'] = default_df.loc[:, 'ade']
-    out_df['classifier_ade'] = boosted_df.loc[:, 'ade']
-    out_df['nn_ade'] = nn_df.loc[:, 'ade']
+    out_df['vanilla_recall'] = default_df.loc[:, 'recall'].round(3)
+    out_df['classic_recall'] = boosted_df.loc[:, 'recall'].round(3)
+    out_df['nn_recall'] = nn_df.loc[:, 'recall'].round(3)
 
-    out_df['fde'] = default_df.loc[:, 'fde']
-    out_df['classifier_fde'] = boosted_df.loc[:, 'fde']
-    out_df['nn_fde'] = nn_df.loc[:, 'fde']
+    # out_df['vanilla_ade'] = default_df.loc[:, 'ade'].round(3)
+    # out_df['classic_ade'] = boosted_df.loc[:, 'ade'].round(3)
+    # out_df['nn_ade'] = nn_df.loc[:, 'ade'].round(3)
+    #
+    # out_df['vanilla_fde'] = default_df.loc[:, 'fde'].round(3)
+    # out_df['classic_fde'] = boosted_df.loc[:, 'fde'].round(3)
+    # out_df['nn_fde'] = nn_df.loc[:, 'fde'].round(3)
 
-    out_df['neighbourhood_radius'] = nn_df['neighbourhood_radius']
 
+    # out_df['neighbourhood_radius'] = nn_df['neighbourhood_radius']
+
+    # out_df.to_latex(f'{save_root_path}final_traj_metrics_for_all_steps.tex', index=False)
+    out_df.to_latex(f'{save_root_path}final_detection_metrics_for_all_steps.tex', index=False)
     out_df.to_csv(f'{save_root_path}final_metrics_for_all_steps.csv', index=False)
 
     if dump_as_markdown:
@@ -96,20 +117,25 @@ def pm_annotation_versions_diff_csv(
     version1_df = pd.read_csv(version1_path)
 
     out_df = version0_df[['class', 'number']]
+    out_df['class'] = version0_df.loc[:, 'class'].str.lower()
 
-    out_df['v0_precision'] = version0_df.loc[:, 'precision']
-    out_df['v1_precision'] = version1_df.loc[:, 'precision']
 
-    out_df['v0_recall'] = version0_df.loc[:, 'recall']
-    out_df['v1_recall'] = version1_df.loc[:, 'recall']
+    out_df['v0_precision'] = version0_df.loc[:, 'precision'].round(3)
+    out_df['v1_precision'] = version1_df.loc[:, 'precision'].round(3)
 
-    out_df['v0_ade'] = version0_df.loc[:, 'ade']
-    out_df['v1_ade'] = version1_df.loc[:, 'ade']
+    out_df['v0_recall'] = version0_df.loc[:, 'recall'].round(3)
+    out_df['v1_recall'] = version1_df.loc[:, 'recall'].round(3)
 
-    out_df['v0_fde'] = version0_df.loc[:, 'fde']
-    out_df['v1_fde'] = version1_df.loc[:, 'fde']
+    # out_df['v0_ade'] = version0_df.loc[:, 'ade'].round(3)
+    # out_df['v1_ade'] = version1_df.loc[:, 'ade'].round(3)
+    #
+    # out_df['v0_fde'] = version0_df.loc[:, 'fde'].round(3)
+    # out_df['v1_fde'] = version1_df.loc[:, 'fde'].round(3)
 
-    out_df['neighbourhood_radius'] = version0_df['neighbourhood_radius']
+    # out_df['neighbourhood_radius'] = version0_df['neighbourhood_radius']
+
+    # out_df.to_latex(f'{save_root_path}final_final_approach_traj.tex', index=False)
+    out_df.to_latex(f'{save_root_path}final_final_approach_detections.tex', index=False)
 
     if dump_as_markdown:
         pm_annotation_versions_diff_save_as_markdown(
@@ -120,8 +146,8 @@ def pm_annotation_versions_diff_csv(
         save_as_tex(
             out_df,
             path=f'{save_root_path}pm_annotation_versions_diff.tex')
-        
-        
+
+
 def pm_annotation_versions_diff_save_as_markdown(out_df, path):
     series_list = []
     for r_idx, row in out_df.iterrows():
@@ -238,23 +264,90 @@ def compare_crop_classifiers():
         index=False)
 
 
+def plot_random_legends():
+    blue_star = lines.Line2D([], [], color='white', marker='*', linestyle='None',
+                             markersize=10, label='Cluster\'s Centroid', markeredgecolor='black')
+    red_circle = lines.Line2D([], [], color='red', marker='o', linestyle='None',
+                              markersize=10, label='Region of Validity', markerfacecolor='white')
+    purple_triangle = lines.Line2D([], [], color='green', marker='o', linestyle='None',
+                                   markersize=10, label='Cluster Pool Region', markerfacecolor='white')
+    purple_triangle0 = lines.Line2D([], [], color='yellow', marker='o', linestyle='None',
+                                    markersize=10, label='Inside Region of Validity')
+    purple_triangle1 = lines.Line2D([], [], color='aqua', marker='o', linestyle='None',
+                                    markersize=10, label='New track candidate activations')
+    # red_square = lines.Line2D([], [], color='red', marker='s', linestyle='None',
+    #                           markersize=10, label='Ground Truth Bounding Box', markerfacecolor='white')
+    # aqua_square = lines.Line2D([], [], color='aqua', marker='s', linestyle='None',
+    #                           markersize=10, label='Generated Bounding Box', markerfacecolor='white')
+
+    # blue_square = lines.Line2D([], [], color='blue', marker='s', linestyle='None',
+    #                           markersize=10, label='Points at (T-1)', markerfacecolor='white')
+    # magenta_square = lines.Line2D([], [], color='magenta', marker='s', linestyle='None',
+    #                           markersize=10, label='Points at T', markerfacecolor='white')
+    # red_square = lines.Line2D([], [], color='red', marker='s', linestyle='None',
+    #                           markersize=10, label='(T-1) Shifted Bounding Box', markerfacecolor='white')
+    # gray_square = lines.Line2D([], [], color='gray', marker='s', linestyle='None',
+    #                           markersize=10, label='Selected XY', markerfacecolor='white')
+    # yellow_square = lines.Line2D([], [], color='yellow', marker='s', linestyle='None',
+    #                           markersize=10, label='Selected XY Current', markerfacecolor='white')
+    # green_square = lines.Line2D([], [], color='green', marker='s', linestyle='None',
+    #                           markersize=10, label='(T-1) OF Shifted Bounding Box', markerfacecolor='white')
+
+    # red_square = lines.Line2D([], [], color='red', marker='s', linestyle='None',
+    #                           markersize=10, label='Pedestrian', markerfacecolor='white')
+    # green_square = lines.Line2D([], [], color='green', marker='s', linestyle='None',
+    #                           markersize=10, label='Bus', markerfacecolor='white')
+    # black_square = lines.Line2D([], [], color='black', marker='s', linestyle='None',
+    #                           markersize=10, label='Biker', markerfacecolor='white')
+    # white_square = lines.Line2D([], [], color='white', marker='s', linestyle='None',
+    #                           markersize=10, label='Car', markerfacecolor='white')
+    # purple_square = lines.Line2D([], [], color='purple', marker='s', linestyle='None',
+    #                           markersize=10, label='Skater', markerfacecolor='white')
+    # aqua_square = lines.Line2D([], [], color='aqua', marker='s', linestyle='None',
+    #                           markersize=10, label='Cart', markerfacecolor='white')
+
+    blue_circle = lines.Line2D([], [], color='blue', marker='s', linestyle='None',
+                              markersize=10, label='Observed', markerfacecolor='white')
+    green_circle = lines.Line2D([], [], color='green', marker='s', linestyle='None',
+                              markersize=10, label='Predicted', markerfacecolor='white')
+    red_circle = lines.Line2D([], [], color='red', marker='s', linestyle='None',
+                              markersize=10, label='Ground Truth', markerfacecolor='white')
+    # aqua_circle = lines.Line2D([], [], color='aqua', marker='o', linestyle='None',
+    #                           markersize=10, label='Last Observed Position', markerfacecolor='white')
+
+    # plt.legend(handles=[blue_star, red_circle, purple_triangle, purple_triangle0, purple_triangle1, red_square])
+    plt.legend(handles=[blue_star, red_circle, purple_triangle, purple_triangle0, purple_triangle1])
+
+    plt.show()
+    print()
+    # fig, axs = plt.subplots(1, 1, sharex='none', sharey='none', figsize=(12, 10))
+    # legends_dict = {'*': 'Cluster\'s Centroid',
+    #                 'g': 'Cluster Pool Region',
+    #                 'r': 'Ground Truth Bounding Box'}
+    #
+    # legend_patches = [patches.Patch(color=key, label=val) for key, val in legends_dict.items()]
+    # fig.legend(handles=legend_patches, loc=2)
+    # plt.show()
+
+
 if __name__ == '__main__':
     # sdd_stats_from_website()
-    d_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/generated_annotations/metrics_2m.csv'
-    b_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/' \
-             'filtered_generated_annotations/metrics_2m.csv'
-    n_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/' \
-             'SDD/classic_nn_extended_annotations_v1/d2/simple/metrics_2m.csv'
-    csv_merge(d_path, b_path, n_path,
-              save_root_path='/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/',
-              dump_as_markdown=True)
-    
-    # v0_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/' \
-    #          'pm_extracted_annotations_v0/metrics.csv'
-    # v1_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/' \
-    #          'pm_extracted_annotations/metrics_2m.csv'
-    # pm_annotation_versions_diff_csv(v0_path, v1_path,
-    #                                 '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/')
+    # plot_random_legends()
+    # d_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/generated_annotations/metrics_2m.csv'
+    # b_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/' \
+    #          'filtered_generated_annotations/metrics_2m.csv'
+    # n_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/' \
+    #          'SDD/classic_nn_extended_annotations_v1/d2/simple/metrics_2m.csv'
+    # csv_merge(d_path, b_path, n_path,
+    #           save_root_path='/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/',
+    #           dump_as_markdown=True)
+
+    v0_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/' \
+             'pm_extracted_annotations_v0/metrics.csv'
+    v1_path = '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/' \
+             'pm_extracted_annotations/metrics_2m.csv'
+    pm_annotation_versions_diff_csv(v0_path, v1_path,
+                                    '/home/rishabh/Thesis/TrajectoryPredictionMastersThesis/Datasets/SDD/')
 
     compare_crop_classifiers()
     load_df_and_diff_save_as_markdown_for_precision_recall(
